67217e258feed99e0f05bff8d0a9588b1234c8b2
ToasterPanic <me@owouw.us>
2026-02-20 14:02:56 -0500

    Initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+@export var walk_speed = 5.0
+@export var gravity = 9.8
+
+@export var mouse_sensitivity = 0.002
+@export var controller_camera_sensitivity = 1.6
+
+@onready var camera = $Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		
+		camera.rotation.x = clamp((camera.rotation.x - (event.relative.y * mouse_sensitivity)), -PI/2, PI/2)
+
+func _handle_controller_camera_input(delta):
+	var input_dir = Input.get_vector("camera_left", "camera_right", "camera_down", "camera_up")
+	
+	
+	rotate_y(-input_dir.x * controller_camera_sensitivity * delta)
+	
+	camera.rotation.x = clamp((camera.rotation.x - (-input_dir.y * controller_camera_sensitivity * delta)), -PI/2, PI/2)
+	
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		
+	_handle_controller_camera_input(delta)
+
+	var input_dir = Input.get_vector("move_backwards", "move_forward", "move_left", "move_right")
+	
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+
+	velocity.x = direction.x * walk_speed
+	velocity.z = direction.z * walk_speed
+
+	move_and_slide()
