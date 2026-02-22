7660cf05b8662f433c7bbdd94fe44a773ffa9a7b
ToasterPanic <me@owouw.us>
2026-02-21 21:02:38 -0500

    Lasers should not send ships flying or not expire

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -2,6 +2,8 @@ extends RigidBody3D
 
 var creator = null
 
+var time = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,7 +11,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	delta += time
+	
+	if time > 10:
+		queue_free()
 	
 func _on_body_entered(body: Node) -> void:
 	if "health" in body:
