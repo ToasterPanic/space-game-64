fcb091048ad6bf3382ea0eb52a78605bd1e91337
ToasterPanic <me@owouw.us>
2026-02-25 13:32:00 -0500

    Character body hitbox for each limb

---
+++ scripts/character_hitbox_setup.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	for n in get_children():
+		get_parent().find_child(n.name + "_1").add_child(n)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
