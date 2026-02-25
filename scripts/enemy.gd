75d09906d0b70f058e44b303ded9a235ce596cf7
ToasterPanic <me@owouw.us>
2026-02-25 17:17:34 -0500

    I FUCKING HATE IMPORTING 2: WHY THE FUCK DO ANIMATIONS HAVE TO BE SUCH A PAIN IN THE ASS!!!!

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -4,8 +4,11 @@ var health = 100
 var texture = load("res://textures/character_test.png")
 
 @onready var mesh = $Mesh
+@onready var animator = $Animator
 
-func _apply_texture(node: Node3D, material: StandardMaterial3D):
+var weapon_equipped = false
+
+func _apply_texture(node: Node, material: StandardMaterial3D):
 	for n in node.get_children():
 		if n.is_class("MeshInstance3D"):
 			n.material_override = material
@@ -15,9 +18,9 @@ func _apply_texture(node: Node3D, material: StandardMaterial3D):
 func _ready() -> void:
 	var material = StandardMaterial3D.new()
 	material.albedo_texture = texture
+	material.texture_filter = BaseMaterial3D.TEXTURE_FILTER_NEAREST
 		
 	_apply_texture(mesh, material)
-		
 
 func _physics_process(delta: float) -> void:
 	velocity -= Vector3(0, 9.8 * delta, 0)
